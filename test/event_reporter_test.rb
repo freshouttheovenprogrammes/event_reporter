af615489e5d98d812837c00f36877cfbb697d903
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-10-24 18:16:47 -0600

    Pushed w/ problem description

--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -24,15 +24,28 @@ class EventReporterRunnerTest < Minitest::Test
 
   def test_that_it_can_read_csv_file
     event_reporter = EventReporter.new
-
-    assert_equal './lib/full_event_attendees.csv', event_reporter.filename
+    expected = event_reporter.load
+    assert_instance_of CSV, expected
   end
 
-  def test_that_its_searching_first_name
+  def test_that_its_searching_first_name_and_that_it_appears_in_downcase
     event_reporter = EventReporter.new
-
-    assert_equal "Allison", event_reporter.search(:first_name, "Allison")
+    # getting error which is posted after test class ends
+    assert "allison", event_reporter.search(:first_name, "allison")
 
   end
 
 end
+
+=begin
+
+1) Error:
+EventReporterRunnerTest#test_that_its_searching_first_name_and_that_it_appears_in_downcase:
+TypeError: no implicit conversion of String into Integer
+    /Users/zacharypalmquist/turing/1module/event_reporter/lib/event_reporter.rb:21:in `[]'
+    /Users/zacharypalmquist/turing/1module/event_reporter/lib/event_reporter.rb:21:in `block in search'
+    /Users/zacharypalmquist/.rvm/rubies/ruby-2.4.2/lib/ruby/2.4.0/csv.rb:1771:in `each'
+    /Users/zacharypalmquist/turing/1module/event_reporter/lib/event_reporter.rb:20:in `search'
+    ./test/event_reporter_test.rb:34:in `test_that_its_searching_first_name_and_that_it_appears_in_downcase'
+
+=end
